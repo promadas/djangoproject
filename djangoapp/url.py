6ce0549cc9c4a5a946a11245df71f8ad560fa86d
Proma Das <annieproma@gmail.com>
2023-02-07 16:18:20 +0600

    First commit

--- djangoapp/url.py
+++ djangoapp/url.py
@@ -1,9 +1,13 @@
-from django.contrib import admin
+'''from django.contrib import admin
 from django.urls import path
-from . import views
+from djangoapp import views
 
 app_name = 'djangoapp'
 
 urlpatterns = [
-    path('login/', views.login, name='login')
+    path('admin/', admin.site.urls),
+    path('register/',views.sign_up,name='sign_up'),
+    path('login/',views.log_in,name='login_page'),
+    #path('profile/',views.profile,name='profile'),
 ]
+'''
--- djangoapp/views.py
+++ djangoapp/views.py
@@ -1,32 +1,48 @@
-from django.shortcuts import render,redirect
-from .login import LoginFormSubmitForm
-from .models import contact
+'''from django.shortcuts import render,redirect,HttpResponse
+from .forms import CustomUserCreationForm
+
+from django.contrib.auth import login,logout,authenticate
+from .models import CustomUser
+
 # Create your views here.
-def login(request):
+
+
+def sign_up(request):
+    
+    detail_list = CustomUser.objects.all().values()
+    if request.method == 'POST':
+        fm = CustomUserCreationForm(request.POST)
+        if fm.is_valid():
+            fm.save()
+            
+            #return redirect(log_in)
+        
+    else:
+        fm = CustomUserCreationForm()
+        return render(request,'sign_up.html',{'form':fm, 'details':detail_list})
+        
+        
+def log_in(request):
     
     if request.method == 'POST':
         
-        form = LoginFormSubmitForm(request.POST)
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = authenticate(request,username=username,password=password)
         
-        if form.is_valid():
-           
-            form.save()
-            
-            return redirect(request.path_info)
+        if user is not None:
+            login(request,user)
+            return redirect(profile)
         
         else:
-            
-            form = LoginFormSubmitForm()
-            return redirect(request.path_info)
-    
-    
+           return HttpResponse("Username or password is incorrect")
+        
     else:
-        form = LoginFormSubmitForm()
-        
-    context = {
-        'form': form ,
-        
-        
-    }
- 
-    return render(request, 'login.html',context) 
+        return render(request,'log_in.html')
+    
+
+def profile(request):
+    return render(request,'profile.html')
+
+
+'''
